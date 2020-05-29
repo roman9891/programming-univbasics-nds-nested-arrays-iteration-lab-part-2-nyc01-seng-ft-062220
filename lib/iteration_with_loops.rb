6e3baab26c328386d31551b87ccfebb96b58695e
Roman Opalacz <roman9891@gmail.com>
2020-05-29 18:04:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def find_min_in_nested_arrays(src)
     lowest_temp_per_day = src[i][0]
     while j < src[i].count do 
       if src[i][j] < lowest_temp_per_day
-        #lowest_temp_per_day = src[i][j]
+        lowest_temp_per_day = src[i][j]
       end
       j += 1 
     end
